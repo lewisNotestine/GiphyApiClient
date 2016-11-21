f492b83dc2a0b27f9eeba460712ce97b50ead6de
Lewis S Notestine <lewis.notestine@gmail.com>
2016-11-21 00:18:40 -0800

    added new Task-based async implementations for API
    Also, fixed some cruft that existed in 1.0.0:
    the client used a bunch of magic strings to construct parameters before.
    Now we put that stuff in a RequestBuilder with some extension methods
    to give us what we need.

--- GiphyApiClient.NetCore/Client/GiphyClient.cs
+++ GiphyApiClient.NetCore/Client/GiphyClient.cs
@@ -3,7 +3,7 @@ using RestSharp;
 using GiphyApiClient.NetCore.Models.Input;
 using GiphyApiClient.NetCore.Models.Output;
 using GiphyApiClient.NetCore.Config;
-using System.Net;
+using System.Threading.Tasks;
 
 namespace GiphyApiClient.NetCore.Client
 {
@@ -23,215 +23,42 @@ namespace GiphyApiClient.NetCore.Client
 
         public RestRequestAsyncHandle SearchAsync(SearchParams searchInput, Action<IRestResponse<MultipleResult>, RestRequestAsyncHandle> callback)
         {
-
-            var restRequest = new RestRequest(resource: "search",
-                method: Method.GET);
-            restRequest.AddParameter(new Parameter()
-            {
-                Name = "q",
-                Value = searchInput.q,
-                Type = ParameterType.QueryString
-            });
-
-
-            if (searchInput.limit.HasValue)
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "limit",
-                    Value = searchInput.limit,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (searchInput.offset.HasValue)
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "offset",
-                    Value = searchInput.offset,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrEmpty(searchInput.rating))
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "rating",
-                    Value = searchInput.rating,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrEmpty(searchInput.lang))
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "lang",
-                    Value = searchInput.lang,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-
-            if (!string.IsNullOrEmpty(searchInput.fmt))
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "fmt",
-                    Value = searchInput.fmt,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            restRequest.AddParameter(new Parameter()
-            {
-                Name = "api_key",
-                Value = Config.ApiKey,
-                Type = ParameterType.QueryString
-            });
-            
+            var restRequest = new RequestBuilder()
+                .GetSearchRequest(searchInput)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+       
             return RestClnt.GetAsync<MultipleResult>(restRequest, callback);            
         }
 
         public RestRequestAsyncHandle TrendingAsync(TrendingParams input, Action<IRestResponse<MultipleResult>, RestRequestAsyncHandle> callback)
         {
-        
-            var restRequest = new RestRequest(resource: "trending",
-                method: Method.GET);
-            if (input.limit.HasValue)
-            {
-                restRequest.AddParameter(new Parameter() 
-                {
-                    Name = "limit",
-                    Value = input.limit.Value,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrEmpty(input.rating))
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "rating",
-                    Value = input.rating,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrEmpty(input.fmt))
-            {
-                restRequest.AddParameter(new Parameter()
-                {
-                    Name = "fmt",
-                    Value = input.fmt,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            restRequest.AddParameter(new Parameter()
-            {
-                Name = "api_key",
-                Value = Config.ApiKey,
-                Type = ParameterType.QueryString
-            });
+            var restRequest = new RequestBuilder()
+                .GetTrendingRequest(input)
+                .WithApiKey(Config.ApiKey)
+                .Build();           
 
            return RestClnt.GetAsync<MultipleResult>(restRequest, callback);
         }
 
         public RestRequestAsyncHandle TranslateAsync(TranslateParams parms, Action<IRestResponse<SingleResult>, RestRequestAsyncHandle> callback)
         {
-            var request = new RestRequest(resource: "translate", method: Method.GET);
-            
-            request.AddParameter(new Parameter()
-            {
-                Name = "s",
-                Value = parms.s,
-                Type = ParameterType.QueryString
-            });
+            var request = new RequestBuilder()
+                .GetTranslateRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
 
-            if (!string.IsNullOrWhiteSpace(parms.rating))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "rating",
-                    Value = parms.rating,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrWhiteSpace(parms.lang))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "lang",
-                    Value = parms.lang,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrWhiteSpace(parms.fmt))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "fmt",
-                    Value = parms.fmt,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            request.AddParameter(new Parameter()
-            {
-                Name = "api_key",
-                Value = Config.ApiKey, 
-                Type = ParameterType.QueryString
-            });
-            
             return RestClnt.GetAsync<SingleResult>(request, callback);
         }
 
         public RestRequestAsyncHandle RandomAsync(RandomParams parms, Action<IRestResponse<RandomResult>, RestRequestAsyncHandle> callback)
         {
-            var request = new RestRequest(resource: "random");
-            if (!string.IsNullOrWhiteSpace(parms.tag))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "tag",
-                    Value = parms.tag,
-                    Type = ParameterType.QueryString
-                });
-            }
+            var request = new RequestBuilder()
+                .GetRandomRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
 
-            if (!string.IsNullOrWhiteSpace(parms.rating))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "rating",
-                    Value = parms.rating,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            if (!string.IsNullOrWhiteSpace(parms.fmt))
-            {
-                request.AddParameter(new Parameter()
-                {
-                    Name = "fmt",
-                    Value = parms.fmt,
-                    Type = ParameterType.QueryString
-                });
-            }
-
-            request.AddParameter(new Parameter()
-            {
-                Name = "api_key",
-                Value = Config.ApiKey,
-                Type = ParameterType.QueryString
-            });
-
-           return RestClnt.GetAsync<RandomResult>(request, callback);
+            return RestClnt.GetAsync<RandomResult>(request, callback);
         } 
 
         public RestRequestAsyncHandle GifByIdAsync(GifByIdParams parms, Action<IRestResponse<SingleResult>, RestRequestAsyncHandle> callback)
@@ -241,20 +68,20 @@ namespace GiphyApiClient.NetCore.Client
                 throw new ArgumentException("id parameter is not optional");
             }
 
-            var request = new RestRequest(resource: parms.id);
-            request.AddParameter(new Parameter()
-            {
-                Name = "api_key", 
-                Value = Config.ApiKey,
-                Type = ParameterType.QueryString
-            });
+            var request = new RequestBuilder()
+                .GetSearchByIdRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
 
             return RestClnt.GetAsync(request, callback);
         }
 
+        ///<remarks>
+        ///There are some problems with this implementation, specifically that it has to new up a special client to get this done.
+        /// The reason is that the <code>ids</code> parameter is a csv format, which RestSharp doesn't seem to like.
+        ///</remarks>
         public RestRequestAsyncHandle GifsByIdAsync(GifsByIdParams parms, Action<IRestResponse<MultipleResult>, RestRequestAsyncHandle> callback)
         {
-
             if (string.IsNullOrWhiteSpace(parms.ids))
             {
                 throw new ArgumentException("ids parameter is not optional");
@@ -262,6 +89,67 @@ namespace GiphyApiClient.NetCore.Client
             var specialClient = new RestClient($"{Config.BaseUrl}?api_key={Config.ApiKey}&ids={parms.ids}");
             var request = new RestRequest();
             return specialClient.GetAsync<MultipleResult>(request, callback);
+        }
+
+        public Task<IRestResponse<MultipleResult>> SearchAsyncTask(SearchParams parms)
+        {
+            var request = new RequestBuilder()
+                .GetSearchRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+            
+            return RestClnt.ExecuteGetTaskAsync<MultipleResult>(request);
+        }
+
+        public Task<IRestResponse<MultipleResult>> TrendingAsyncTask(TrendingParams parms)
+        {
+            var request = new RequestBuilder()
+                .GetTrendingRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+            
+            return RestClnt.ExecuteGetTaskAsync<MultipleResult>(request);
+        }
+
+        public Task<IRestResponse<SingleResult>> TranslateAsyncTask(TranslateParams parms)
+        {
+            var request = new RequestBuilder()
+                .GetTranslateRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+            
+            return RestClnt.ExecuteGetTaskAsync<SingleResult>(request);
+        }
+
+        public Task<IRestResponse<RandomResult>> RandomAsyncTask(RandomParams parms)
+        {
+            var request = new RequestBuilder()
+                .GetRandomRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+
+            return RestClnt.ExecuteGetTaskAsync<RandomResult>(request);
+        }
+
+        public Task<IRestResponse<SingleResult>> GifByIdAsyncTask(GifByIdParams parms)
+        {
+            var request = new RequestBuilder()
+                .GetSearchByIdRequest(parms)
+                .WithApiKey(Config.ApiKey)
+                .Build();
+            
+            return RestClnt.ExecuteGetTaskAsync<SingleResult>(request);
+        }
+
+        public Task<IRestResponse<MultipleResult>> GifsByIdsAsyncTask(GifsByIdParams parms)
+        {
+            if (string.IsNullOrWhiteSpace(parms.ids))
+            {
+                throw new ArgumentException("ids parameter is not optional");
+            }
+            var specialClient = new RestClient($"{Config.BaseUrl}?api_key={Config.ApiKey}&ids={parms.ids}");
+            var request = new RestRequest();
+            return specialClient.ExecuteGetTaskAsync<MultipleResult>(request);
         }
     }
 }
